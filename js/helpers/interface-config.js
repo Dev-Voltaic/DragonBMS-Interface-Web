006683325a9bdaf15d3c4e4aa659c5b513e93f76
Peter <hi@p3h3.de>
2023-01-18 18:08:52 +0100

    working color pickers for interface config

--- js/helpers/interface-config.js
+++ js/helpers/interface-config.js
@@ -40,3 +40,44 @@ function updateConfig(){
 
 
 updateConfig();
+
+
+
+
+
+
+
+
+/** Default configuration **/
+
+Coloris({
+    theme: 'polaroid',
+});
+
+
+/** Default configuration **/
+
+Coloris({
+    el: '.coloris',
+    swatches: [
+        '#264653',
+        '#2a9d8f',
+        '#e9c46a',
+        '#f4a261',
+        '#e76f51',
+        '#d62828',
+        '#023e8a',
+        '#0077b6',
+        '#0096c7',
+        '#00b4d8',
+        '#48cae4'
+    ]
+});
+
+Coloris.setInstance('.foreground-color', {
+    theme: 'polaroid'
+});
+Coloris.setInstance('.background-color', {
+    theme: 'polaroid'
+});
+
